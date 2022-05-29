015ed194c02277beac7afc174a3b46e900bd64b7
29DD <aw80127@gmail.com>
2022-05-29 16:46:59 +0800

    time

--- js/loading/loading.js
+++ js/loading/loading.js
@@ -1,4 +1,4 @@
 $(window).load(function () { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-    $("#status").delay(1000).fadeOut(9000); //delay --> 延遲幾秒才fadeOut
+    $("#status").delay(1000).fadeOut(7000); //delay --> 延遲幾秒才fadeOut
     $("#preloader").delay(3000).fadeOut(3000);
 })
