d6f9adf47bfe4ccae356b26f407f4e41548065a2
29DD <aw80127@gmail.com>
2022-05-30 09:28:11 +0800

    456

--- js/loading/loading.js
+++ js/loading/loading.js
@@ -1,20 +1,7 @@
 
-<<<<<<< HEAD
-$(window).load(function () { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-    $("#status").delay(3000).fadeOut(5000); //delay --> 延遲幾秒才fadeOut
-    $("#preloader").delay(3000).fadeOut(5000);
-})
-
-// $(window).load(function () {
-//     $("#status").delay(5000).fadeOut(3000); fadeOut
-//     $("#preloader").delay(7000).fadeOut(3000);
-// })
-=======
-
 $(window).load(function () { // 確認整個頁面讀取完畢再將這三個div隱藏起來
         $("#status").delay(500).fadeOut(2000); //delay --> 延遲幾秒才fadeOut
         $("#preloader").delay(500).fadeOut(2000);
 })
 
 
->>>>>>> 2cc87d864d7da36ed8ebeac55e93a911f571b1f2
