2c5bd590cd071838f3db9e13985b4de02a6714ef
29DD <aw80127@gmail.com>
2022-05-30 09:24:29 +0800

    123

--- js/loading/loading.js
+++ js/loading/loading.js
@@ -1,4 +1,10 @@
+
 $(window).load(function () { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-    $("#status").delay(5000).fadeOut(3000); //delay --> 延遲幾秒才fadeOut
-    $("#preloader").delay(7000).fadeOut(3000);
+    $("#status").delay(3000).fadeOut(5000); //delay --> 延遲幾秒才fadeOut
+    $("#preloader").delay(3000).fadeOut(5000);
 })
+
+// $(window).load(function () {
+//     $("#status").delay(5000).fadeOut(3000); fadeOut
+//     $("#preloader").delay(7000).fadeOut(3000);
+// })
